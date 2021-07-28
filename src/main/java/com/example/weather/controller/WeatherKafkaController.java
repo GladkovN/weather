09ca6df74c61786184f78ded9664c3ef664a793d
GladkovN <n.gladkov.salut@mail.ru>
2021-07-28 15:23:35 +0300

    test task 3 3 commit create user service

--- src/main/java/com/example/weather/controller/WeatherKafkaController.java
+++ src/main/java/com/example/weather/controller/WeatherKafkaController.java
@@ -1,14 +1,15 @@
 package com.example.weather.controller;
 
+import com.example.weather.model.Weather;
+import com.example.weather.service.UserService;
 import com.example.weather.service.WeatherRequestService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -17,9 +18,18 @@ public class WeatherKafkaController {
     @Autowired
     WeatherRequestService weatherRequestService;
 
-    @GetMapping
-    public ResponseEntity<String> getWeather(@RequestParam String city) throws JsonProcessingException {
-        weatherRequestService.requestWeather( city );
+    @Autowired
+    UserService userService;
+
+    @PostMapping
+    public ResponseEntity<String> saveWeather(@RequestParam String city) throws JsonProcessingException {
+        weatherRequestService.requestWeather(city);
         return ResponseEntity.status(HttpStatus.OK).body("Ok");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Weather>> getWeather() {
+        List<Weather> weatherList = userService.getAllWeather();
+        return ResponseEntity.status(HttpStatus.OK).body(weatherList);
     }
 }
---
+++ src/main/java/com/example/weather/service/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package com.example.weather.service;
+
+import com.example.weather.model.Weather;
+import com.example.weather.repository.WeatherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class UserServiceImpl implements UserService{
+    @Autowired
+    private WeatherRepository weatherRepository;
+
+    @Override
+    public List<Weather> getAllWeather() {
+        return weatherRepository.findAll();
+    }
+}
